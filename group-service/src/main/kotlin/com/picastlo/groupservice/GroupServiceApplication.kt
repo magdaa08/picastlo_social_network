a590f78432065cf48cf0bbfa3e143b5b683ff0b3
magdaa08 <madzia0804@gmail.com>
2024-10-26 22:09:51 +0200

    Initial fixed server structure

---
+++ group-service/src/main/kotlin/com/picastlo/groupservice/GroupServiceApplication.kt
@@ -0,0 +1,11 @@
+package com.picastlo.groupservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GroupServiceApplication
+
+fun main(args: Array<String>) {
+    runApplication<GroupServiceApplication>(*args)
+}
