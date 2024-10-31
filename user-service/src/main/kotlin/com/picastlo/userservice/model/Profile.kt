e30e5faeaf1c48b3e8352f0cdb3e0290a69b0d8b
magdaa08 <madzia0804@gmail.com>
2024-10-31 15:59:52 +0100

    Added entity Profile to user-service

--- user-service/src/main/kotlin/com/picastlo/userservice/model/Profile.kt
+++ user-service/src/main/kotlin/com/picastlo/userservice/model/Profile.kt
@@ -1,4 +1,14 @@
-package com.picastlo.userservice.model
+package com.picastlo.userservice.entity
 
-class Profile {
-}
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "profiles")
+data class Profile(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @Column(name = "user_id")
+    val userId: Long,
+    val bio: String? = null,
+    val avatar: String? = null
+)
