40fb49442366331fc4c971971b9fab0e78a0533c
magdaa08 <madzia0804@gmail.com>
2024-11-09 19:14:44 +0100

    Added Pipeline Entity

--- pipeline-service/src/main/kotlin/com/picastlo/pipelineservice/presentation/model/Pipeline.kt
+++ pipeline-service/src/main/kotlin/com/picastlo/pipelineservice/presentation/model/Pipeline.kt
@@ -1,5 +1,28 @@
-package com.picastlo.pipelineservice.model
+package com.picastlo.pipelineservice.presentation.model
 
-class Pipeline {
-    //TODO
-}
+import jakarta.persistence.*
+import java.util.*
+
+@Entity
+@Table(name = "pipelines")
+data class Pipeline(
+    @Id
+    @GeneratedValue
+    val id: Long? = null,
+
+    @Column(nullable = false)
+    val name: String,
+
+    val description: String? = null,
+
+    @Column(nullable = false)
+    val ownerId: Long,
+
+    @Lob
+    @Column(nullable = false)
+    val transformations: String,
+
+    @Lob
+    val initialImage: ByteArray? = null,
+
+)
